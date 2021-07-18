c81ddb676f57ed70cef438cb428264e7211fdfd9
MattZ105 <72571184+MattZ105@users.noreply.github.com>
2021-07-18 01:18:48 -0400

    Commit

---
+++ hw2-sprint1.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std; 
+class CarRental  
+CarDetail(){
+    string Type; 
+    string Brand;
+    string Number; 
+    string Capacity; 
+    cout << "Type: Truck";
+    cout << "Brand: Ford";
+    cout << "Number: F150";
+    cout << "Capacity:10";
+} 
+int main(){
+
+    return 0;
+}
+int Number(){
+    cout << Number; 
+}
+string Type (){
+    cout << Type;
+}
+int Capacity(){
+    cout << Capacity;
+}
+string Brand(){
+    cout << Brand; 
+}
